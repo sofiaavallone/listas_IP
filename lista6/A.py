7069f834e53c79fcde8b0fde9922715c409af714
Sofia Avallone Sakovitz <sas3@windows.cin.ufpe.br>
2025-11-28 15:58:08 -0300

    final-A / meio-B

--- lista6/A.py
+++ lista6/A.py
@@ -3,7 +3,6 @@
 # quartos >= quartos_min
 # preço <= orçamento_max
 catalogo = {}
-scores = []
 
 # Função para adicionar casas no catálogo de Phil
 def catalogando(n, catalogo):
@@ -16,9 +15,41 @@ def catalogando(n, catalogo):
         quartos = quartos_preco[0]
         preco = quartos_preco[1]
 
-        catalogo.update({endereco: {'bairro': bairro, 'quartos': quartos, 'preco': preco}})
+        catalogo.update({endereco: {'bairro': bairro, 'quartos': quartos, 'preço': preco}})
 
     return catalogo
+
+def validacao(catalogo, requisitos):
+    lista = []
+    lista_keys = list(catalogo.keys()) # Lista dos endereços
+    i = 0
+    for info in catalogo.values():
+        quartos = int(info['quartos'])
+        preco = int(info['preço'])
+
+        if quartos >= requisitos[0] and preco <= requisitos[1]: # Lista com as casas válidas e seus respectivos scores
+            score_total = quartos * 10
+            lista.append((lista_keys[i], score_total))
+        
+        i+=1
+
+    return lista
+
+def analise_melhor_casa(scores, catalogo):
+    melhor_casa = ""
+    melhor_score = 0
+    for j in range(len(scores)):
+        #endereco = scores[j][0]
+        #bairro = catalogo[bairro]['bairro']
+        #score_total = scores[j][1]
+
+        if melhor_score < scores[j][1]:
+            melhor_casa = scores[j][0] # Endereço
+            melhor_score = scores[j][1] # Score
+
+    bairro = catalogo[melhor_casa]['bairro'] # Bairro
+
+    return melhor_casa, melhor_score, bairro
 
 print("Phil, querido... Você tem certeza que essa música é literalmente sobre... casas?")
 print("A própria Sabrina disse que nada na música é uma metáfora! Além disso, o sobrenome dela é carpinteira, acho que ela tem lugar de fala…")
@@ -29,29 +60,50 @@ catalogo_completo = catalogando(n_propriedades, catalogo)
 print("Catálogo concluído! Quem será que irá comprar uma casa de Phil?")
 print()
 
+vendas = 0
 nome_cliente = input()
 while nome_cliente != "FIM":
     # (quartos_min, orçamento_max)
+    requisitos = input().split("-")
     tupla_requisitos = (int(requisitos[0]), int(requisitos[1]))
 
-    lista_keys = list(catalogo.keys())
-    i = 0
-    for info in catalogo.values():
-        quartos = info['quartos']
-        preco = info['preço']
-
-        if quartos >= tupla_requisitos[0] and preco <= tupla_requisitos[1]:
-            score_total = quartos * 10
-            scores.append((lista_keys[i], score_total))
-        
-        i+=1
+    scores = validacao(catalogo, tupla_requisitos)
 
     if len(scores) == 0: # Não tem casas válidas
         print(f"Puxa, {nome_cliente}, vou te avisar se algo aparecer. Não tenho nada com esses requisitos.")
         print()
-    #else:
-        # print das casas válidas
-    # Bubble sort para achar o maior score (com opção de empate)
+    else:
+        tupla_melhor_casa = analise_melhor_casa(scores, catalogo) # (endereço, score, bairro)
+
+        print(f"🎤 Bem-vindo ao House Tour de {tupla_melhor_casa[2]}, {nome_cliente}!")
+        print(f"➡ Casa: {tupla_melhor_casa[0]}")
+        print(f"💖 Score: {tupla_melhor_casa[1]} pontos")
+        print()
+
+        if tupla_melhor_casa[1] >= 40:
+            if nome_cliente == "Sabrina Carpenter":
+                print('"Uau, Phil! Acho que finalmente encontrei o cenário perfeito para o clipe de House Tour!"')
+            elif nome_cliente == "Taylor Swift":
+                print('"Essa casa é perfeita para passar as férias na praia!"')
+            else:
+                print(f'"{nome_cliente} ficou encantado(a)! Phil comemora mais uma venda de sucesso!"')
+            print()
+            print('Venda concluída! Phil dança triunfante ao som de "House Tour"!')
+            vendas+=1
+        else:
+            if nome_cliente == "Sabrina Carpenter":
+                print('"Hmm... Sabe Phil, a letra não era tão literal assim…"')
+            elif nome_cliente == "Taylor Swift":
+                print('"Nós nunca vamos comprar essa casa juntos, Phil!"')
+            else:
+                print('"Parece que a música não ajudou nas vendas dessa vez…"')
+            print()
+            print('Talvez a Sabrina realmente não estivesse falando de imóveis…')
+        print()
 
     nome_cliente = input()
-    requisitos = input().split("-")
+
+# Relatório Final
+print("===== RELATÓRIO DE VENDAS =====")
+print(f"Total de casas vendidas: {vendas}")
+print("===============================")
---
+++ lista6/B.py
@@ -0,0 +1,71 @@
+# Categoria: item principal: quantidade
+
+def checagem_especifica(palavra):
+    retorno = exigencias.get(palavra, False)
+    if retorno != False:
+        if retorno[0] == "Gloss":
+            if retorno[1] <= 0:
+                print("TUDO! O Gloss tá on. O look de Glinda tá salvo!")
+            else:
+                print("CADÊ meu gloss? Como divarei? ... A Glinda tá chorando de raiva!")
+        else:
+            if retorno[1] <= 0:
+                print("Latte gelado pronto! A voz de Glinda está salva. Pode vir o próximo take")
+            else:
+                print("Cadeia neles! Faltou o Mimo Sagrado. Essa equipe tá perdida!")
+            
+exigencias = {}
+
+# Fase 1
+exigencia = input().split(": ")
+while len(exigencia) == 3:
+    if exigencia[0] == "Bebidas" and exigencia[1] == "latte":
+        exigencia[2] = int(exigencia[2]) + 1
+    exigencias.update({exigencia[0]: (exigencia[1], exigencia[2])})
+
+    exigencia = input().split(": ")
+
+# Fase 2
+reabastecimento = input().split()
+while len(reabastecimento) == 8:
+    categoria = reabastecimento[5]
+    quantidade = int(reabastecimento[1])
+
+    # Lógica de estoque
+    retorno_get = exigencias.get(categoria, False)
+
+    if retorno_get != False:
+        tupla_inicial = exigencias[categoria]
+        tupla_atualizada = (tupla_inicial[0], int(tupla_inicial[1]) - quantidade)
+        exigencias.update({categoria: tupla_atualizada})
+
+    reabastecimento = input().split()
+
+# Relatório Final
+estoque_negativo = 0
+print("Relatório de Balanço Final:")
+for categoria, tupla in exigencias.items():
+    item = tupla[0]
+    quantidade = int(tupla[1])
+
+    # Frase
+    if quantidade <= 0:
+        frase = "Você entregou TUDO! O mimo tá mais que garantido."
+    else:
+        frase = f"Golpe BAIXÍSSIMO! Faltam {quantidade} mimos. Corre!"
+        estoque_negativo+=1
+
+    print(f"Categoria: {categoria} Item: {item} Status: {frase}")
+print()
+
+# Checagem específicas
+checagem_especifica('Maquiagem')
+checagem_especifica('Bebidas')
+
+print()
+# Veredito final
+print("Veredito Final")
+if estoque_negativo >= 3:
+    print("Thank U, Next! A equipe de camarim foi demitida!")
+else:
+    print("Estoque Aprovado! Glinda vai brilhar em Wicked!")
